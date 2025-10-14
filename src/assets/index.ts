8b57331ee20224ce716e0fe22f76974086d71fca
Mosacd <levanmosiashvili5@gmail.com>
2025-10-15 01:34:04 +0400

    urgent fix

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -21,18 +21,18 @@ export { default as kiuCardImg } from "./KIU.jpg";
 export { default as newsHeroImage } from "./image.png";
 export { default as ProgramHeroImage } from "./programpage.jpg";
 export { default as ProjectsHeroImage } from "./projects.png";
-export { default as ProjectsImage1} from "./advacning.jpg";
-export { default as ProjectsImage2} from "./youth uni.png";
-export { default as YouthUni} from "./youthuni.jpg";
-export { default as YouthUniCard} from "./YouthUniCard.png";
-export { default as FrontiersHero} from "./FrontiersHero.png";
-export { default as FrontiersConf} from "./MathConference.png";
-export { default as Wolfgang} from "./wolfgang.jpg";
+export { default as ProjectsImage1 } from "./advacning.jpg";
+export { default as ProjectsImage2 } from "./youth uni.png";
+export { default as YouthUni } from "./youthuni.jpg";
+export { default as YouthUniCard } from "./YouthUniCard.png";
+export { default as FrontiersHero } from "./FrontiersHero.png";
+export { default as FrontiersConf } from "./MathConference.png";
+export { default as Wolfgang } from "./wolfgang.jpg";
 export { default as ProgramsHeroImage } from "./programsHero.jpg";
 export { default as aboutB } from "./aboutB.png";
 export { default as aboutU } from "./aboutT.png";
 export { default as aboutR } from "./aboutR.png";
-export { default as VacanciesHero } from "./vacancies.jpg";
+export { default as VacanciesHero } from "./Vacancies.jpg";
 
 //program images
 export { default as bechalor } from "./bechalors.png";
